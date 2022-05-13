00b5cdfa50f5c6fc1216a1da33b06c30b4d9e1aa
Alonzo Machiraju <machthree@gmail.com>
2022-05-12 19:47:56 -0700

    Form inputs added

--- src/components/Add.js
+++ src/components/Add.js
@@ -7,8 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import { SERVER_URL } from "../constants.js";
 
 class Add extends React.Component {
@@ -20,6 +19,18 @@ class Add extends React.Component {
   render() {
     return (
       <div align="left">
+        <div>
+          <div>
+            <TextField id="name" label="Name" variant="standard" />
+          </div>
+          <div>
+            {" "}
+            <TextField id="duedate" label="Due Date" variant="standard" />
+          </div>
+          <div>
+            <TextField id="course" label="Course" variant="standard" />
+          </div>
+        </div>
         <Button
           component={Link}
           to={{ pathname: "/" }}
