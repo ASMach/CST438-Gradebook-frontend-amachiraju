b63147b0516528b81c70d184e130f5bcadaadbde
Alonzo Machiraju <machthree@gmail.com>
2022-06-06 19:45:17 -0700

    URI for Assignment route

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://cst438gradeook-backend.herokuapp.com/";
+//export const SERVER_URL = "https://cst438gradeook-backend.herokuapp.com/";
+export const SERVER_URL = "http://localhost:8081/";
